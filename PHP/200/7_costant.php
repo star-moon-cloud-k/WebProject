8ae0dca8eadcb8b596e187bce8e1508d825a97da
star-moon-cloud-k <kspecial44@gmail.com>
2022-06-18 21:15:51 +0900

    php200_입문

---
+++ PHP/200/7_costant.php
@@ -0,0 +1,10 @@
+<?php 
+    //상수 FAVORITE_DOLL에 값 gelatoni를 대입
+    define ("FAVORITE_DOLL", "gelatoni");
+    echo "상수 FAVORITE_DOLL의 값은 ".FAVORITE_DOLL."<br>";
+
+    //상수 FAVORITE_DOLL에 값 duffy를 대입
+    // define ("FAVORITE_DOLL", "duffy");  //에러 발생
+    echo "상수 FAVORITE_DOLL의 값은 ".FAVORITE_DOLL;
+
+?>
---
+++ PHP/200/8_operator.php
@@ -0,0 +1,22 @@
+<?php 
+
+    //더하기
+    $sum = 5+ 5;
+    echo "5 더하기 5는".$sum;
+    echo "<br>";
+
+    $minus  = 5 -2;
+    echo "5 빼기 2는".$minus."<br>";
+
+    //곱하기
+    $mul = 5 * 3;
+    echo "5 곱하기 3은 ".$mul."<br>";
+
+    //나누기 
+    $division = 6 / 3;
+    echo "6나누기 3은 ".$division."<br>";
+
+    //나머지
+    $rest = 5 % 3; //5 % 3 을 연산한 값이 변수  rest 에 대입
+    echo "5 나누기 3의 나머지 값은 ".$rest;
+?>
