ee84fe5a36c3d118f52338d85c1983336355f095
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-01 11:09:24 +0900

    Operator

--- react/react200/src/App.js
+++ react/react200/src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Es6 from "./Es6";
-import Variable from "./Variable";
+import Es6 from "./component/Es6";
+import Variable from "./component/Variable";
+import SpreadOperator from "./SpreadOperator";
 
 function App() {
     return (
         <div>
             <h1>Start React 200!</h1>
             <p>CSS 적용하기</p>
-            <Variable/>
-            {/*<Es6/>*/}
+            <SpreadOperator/>
 
         </div>
     );
