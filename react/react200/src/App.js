b399bf13010726f45413bf23c67e0b33d099cae4
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-01 13:05:55 +0900

    function

--- react/react200/src/App.js
+++ react/react200/src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import React from 'react';
 import Es6 from "./component/Es6";
 import Variable from "./component/Variable";
-import SpreadOperator from "./SpreadOperator";
+import SpreadOperator from "./component/SpreadOperator";
+import ClassPrototype from "./component/ClassPrototype";
+import ArrowFunction from "./component/ArrowFunction";
 
 function App() {
     return (
         <div>
             <h1>Start React 200!</h1>
             <p>CSS 적용하기</p>
-            <SpreadOperator/>
-
+            <ClassPrototype/>
+            <ArrowFunction/>
         </div>
     );
 }
