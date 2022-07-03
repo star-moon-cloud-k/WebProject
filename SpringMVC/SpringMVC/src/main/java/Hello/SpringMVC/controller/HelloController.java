b0f10a2eb13a3b6109479b04dcd3cd9a842b7934
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-03 21:55:30 +0900

    test code

--- SpringMVC/SpringMVC/src/main/java/Hello/SpringMVC/controller/HelloController.java
+++ SpringMVC/SpringMVC/src/main/java/Hello/SpringMVC/controller/HelloController.java
@@ -3,6 +3,9 @@ package Hello.SpringMVC.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,5 +13,37 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data" , "hello!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam ("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;           //json 방식으로 전달하게 된다
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
