b957c8533b9c247c11c3f1f8c8ad85ee4fbe7108
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-12 00:42:37 +0900

    setState

--- react/react200/src/Component/Counter.js
+++ react/react200/src/Component/Counter.js
@@ -5,18 +5,34 @@ class Counter extends Component {
         super(props);
         //state 초깃값 설정
         this.state = {
-            number: 0
+            number: 0,
+            fixedNumber : 0
         };
     }
 
     render() {
-        const {number} = this.state;        //state를 조회할 때는 this.state로 조회
+        const {fixedNumber , number} = this.state;        //state를 조회할 때는 this.state로 조회
         return (
             <div>
                 <h1>{number}</h1>
+                <h2>바뀌지 않는 값{fixedNumber}</h2>
                 <button
                     onClick={() => {
-                        this.setState({number: number + 1});
+                        this.setState({number: number + 1},
+                            () =>{
+                            console.log("방금 setState가 호출되었습니다.");
+                            console.log(this.state);
+                            });
+                        this.setState(prevState =>{
+                        return {
+                            number: prevState.number + 1
+                        };
+                    });
+                        //위 코드와 아래 코드는 완전히 똑같은 기능을 한다.
+                        //아래 코드는 함수에서 바로 객체를 반환한다느 의미이다.
+                        this.setState(prevState =>({
+                            number: prevState.number +1
+                        }));
                     }}
                 >+1
                 </button>
