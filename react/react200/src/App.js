9e6b718d31788930dcd9285f42b654c0a69a17eb
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-11 22:14:31 +0900

    조건부 연산자

--- react/react200/src/App.js
+++ react/react200/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-import JQuery from "./begginer/component/JQuery";
-import ForceUpdate from "./basic/component/ForceUpdate"
-import ComponentClass from './basic/component/ComponentClass'
-import PureComponentClass from "./basic/component/PureComponentClass";
-import ShallowEqual from "./basic/component/ShallowEqual";
 
 
 function App() {
+    const name = '리액트'
     return (
         <div>
-            <h1>Start React 200!</h1>
-            <p>CSS 적용하기</p>
-            <JQuery/>
-            <ComponentClass/>
-            <PureComponentClass/>
-            <ShallowEqual/>
+            {name === '리액트'? (
+              <h1>리액트입니다.</h1>
+            ) : (
+                <h2>리액트가 아닙니다.</h2>
+            )}
         </div>
+
     );
 }
 
