1ff51655517bad98d7974010716764e8b6a45b06
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-12 00:53:25 +0900

    useState

--- react/react200/src/App.js
+++ react/react200/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import MyComponent from "./Component/MyComponent";
 import Counter from "./Component/Counter";
+import Say from "./Component/Say";
 
 const App = () => {
     const age = 5;
     return (<>
         <Counter/>
+            <Say/>
         </>
     )
 
