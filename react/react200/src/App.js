c6a8c523515fbf28a26ab598c98710d6e77af145
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-11 22:15:54 +0900

    조건부 연산자

--- react/react200/src/App.js
+++ react/react200/src/App.js
@@ -2,17 +2,8 @@ import React from 'react';
 
 
 function App() {
-    const name = '리액트'
-    return (
-        <div>
-            {name === '리액트'? (
-              <h1>리액트입니다.</h1>
-            ) : (
-                <h2>리액트가 아닙니다.</h2>
-            )}
-        </div>
-
-    );
+    const name = '리액트x'
+    return <div>{name === '리액트' ? <h1>리액트 입니다.</h1> : null}</div>
 }
 
 export default App;
