77e8f793bd087ad14a22f2b623fec6c2cf27da6a
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-12 01:36:56 +0900

    onChange+setState

--- react/react200/src/Component/EventPractice.js
+++ react/react200/src/Component/EventPractice.js
@@ -1,22 +1,32 @@
 import React, {Component} from "react";
 
-class EventPractice extends Component{
-    render() {
-        return(
-        <div>
-            <h1>이벤트 연습</h1>
-            <input type="text"
-                   name = "message"
-                   placeholder= "아무거나 입력해보세요"
-                   onChange = {
-                       (e) =>{
-                           console.log(e.target.value);
-                       }
-                   }
-            />
-        </div>
+class EventPractice extends Component {
 
-        )
+    state = {
+        message: ''
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>이벤트 연습</h1>
+                <h2>{this.state.message}</h2>
+                <input
+                    type="text"
+                    name="message"
+                    placeholder="아무거나 입력해보세요"
+                    value={this.state.message}
+                    onChange={
+                        (e) => {
+                            this.setState({
+                                message: e.target.value
+                            })
+                        }}
+                />
+
+            </div>
+
+        );
     }
 }
 
