8d983c7731fb43408aaafbb6c108ac3bda8057a6
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-12 01:30:18 +0900

    onChange+target.value

--- react/react200/src/App.js
+++ react/react200/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import MyComponent from "./Component/MyComponent";
 import Counter from "./Component/Counter";
 import Say from "./Component/Say";
+import EventPractice from "./Component/EventPractice";
 
 const App = () => {
     const age = 5;
     return (<>
         <Counter/>
             <Say/>
+            <EventPractice/>
         </>
     )
 
