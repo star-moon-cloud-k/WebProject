298f840fb3ba7e560fdabada0719a5bef3dc1fb9
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-14 23:03:57 +0900

    Validation

--- react/react200/src/App.js
+++ react/react200/src/App.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import MyComponent from "./Component/MyComponent";
 import Counter from "./Component/Counter";
 import Say from "./Component/Say";
 import EventPractice from "./Component/EventPractice";
 import EventPracticeComponent from "./Component/EventPracticeComponent";
-
-const App = () => {
-    const age = 5;
-    return (<>
-        <Counter/>
-            <Say/>
-            <EventPractice/>
-            <EventPracticeComponent/>
-        </>
-    )
-
+import ValidationSample from "./validation/ValidationSample";
+class App extends Component{
+    render() {
+        return (<>
+                <ValidationSample/>
+                <Counter/>
+                <Say/>
+                <EventPractice/>
+                <EventPracticeComponent/>
+            </>
+        )
+    }
 
 }
 
