66230afe8ca4b6465efc99449daf6a5597ec5a35
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-14 22:40:40 +0900

    event handler

--- react/react200/src/Component/EventPracticeComponent.js
+++ react/react200/src/Component/EventPracticeComponent.js
@@ -1,17 +1,27 @@
 import React, {useState} from "react";
 
 const EventPracticeComponent = () => {
-    const [username, setUsername] = useState('');
-    const [message, setMessage] = useState('');
-    const onChangeUsername = e => setUsername(e.target.value);
-    const onChangeMessage = e => setMessage(e.target.value);
-    const onClick = () => {
-        alert(username + ':' + message);
-        setUsername('');
-        setMessage('');
+    const [form, setForm] = useState({
+        username: '',
+        message: ''
+    });
+    const {username , message} = form;
+    const onChange = e => {
+        const nextForm = {
+            ...form, // 기존의 form 내용을 이 자리에 복사한 뒤
+            [e.target.name]: e.target.value   //원하는 값을 덮어 씌우기
+        };
+        setForm(nextForm);
     };
-    const onKeyPress = e => {
-        if (e.key === 'Enter') {
+    const onClick = () =>{
+        alert(username + ': ' + message);
+        setForm({
+            username: '',
+            message: '',
+        });
+    };
+    const  onKeyPress = e=>{
+        if (e.key === 'Enter'){
             onClick();
         }
     };
@@ -20,24 +30,23 @@ const EventPracticeComponent = () => {
         <div>
             <h1>이벤트 연습</h1>
             <input
-                type='text'
-                name='username'
-                placeholder='사용자명'
-                value={username}
-                onChange={onChangeUsername}
-            />
+                type = 'text'
+                name = 'username'
+                placeholder= '사용자명'
+                value  = {username}
+                onChange={onChange}
+                />
             <input
-                type='text'
-                name='message'
-                placeholder='아무거나 입력하세요'
+                type ="text"
+                name = 'message'
+                placeholder= '아무거나 입력해보세요'
                 value={message}
-                onChange={onChangeMessage}
+                onChange={onChange}
                 onKeyPress={onKeyPress}
-            />
-
-            <button onClick = {onClick}>확인</button>
+                />
+            <button onClick ={onClick} >확인</button>
         </div>
     );
-}
+};
 
 export default EventPracticeComponent;
