b92e77b144a28b341a70f19c12c70a6cd37ba702
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-17 19:42:48 +0900

    iteration

--- react/react200/src/Component/IterationSample.js
+++ react/react200/src/Component/IterationSample.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import {number} from "prop-types";
+import React, {useState} from 'react'
 
-const IterationSample = () =>{
 
-    const names = ['눈사람', '얼음', '눈', '바람']
-    const nameList = names.map((name, index) => <li key = {index}>{name}</li>)
+const IterationSample = () => {
+    const [names, setNames] = useState([
+        {id: 1, text: '눈사람'},
+        {id: 2, text: '얼음'},
+        {id: 3, text: '눈'},
+        {id: 4, text: '바람'}
+    ]);
 
-    return(
-        <ul>{nameList}</ul>
-    )
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5);    //새로운 항목을 추가할 때 사용
+
+    const nameList = names.map(name => <li key = {name.id}>{name.text}</li>);
+    return <ul>{nameList}</ul>;
 }
 
 export default IterationSample;
