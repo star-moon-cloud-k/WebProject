6891777eb9f65a7f0e802dc5d2cd2decc6844abe
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-22 01:51:08 +0900

    Todo list

--- react/todo-app/src/App.js
+++ react/todo-app/src/App.js
@@ -5,15 +5,68 @@ import TodoInsert from "./components/TodoInsert";
 
 import {MdCached} from "react-icons/md";
 import TodoList from "./components/TodoList";
+import {useCallback, useRef, useState} from "react";
 
 const App = () => {
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            text: '리액트 기초 알아보기',
+            checked: true,
+        },
+        {
+            id: 2,
+            text: '컴포넌트 스타일링 해보기',
+            checked: true,
+        },
+        {
+            id: 3,
+            text: '일정관리 앱 만들기',
+            checked: false,
+        },
+    ]);
+
+    //교윳값으로 사용될 id
+    //ref를 사용하여 변수 담기
+    const nextId = useRef(4)
+
+    const onInsert = useCallback(
+        text => {
+            const todo = {
+                id: nextId.current,
+                text,
+                checked: false
+            };
+            setTodos(todos.concat(todo));
+            nextId.current += 1; //nextId 1씩 더하기
+        },
+        [todos],
+    );
+
+    const onRemove = useCallback(
+        id => {
+            setTodos(todos.filter(todo => todo.id !== id));
+        },
+        [todos],
+    )
+
+    const onToggle = useCallback(
+        id => {
+            setTodos(
+                todos.map(todo =>
+                    todo.id === id ? {...todo, checked: !todo.checked} : todo,
+                ),
+            );
+        },
+        [todos],
+    );
+
     return (
         <TodoTemplate>
-            <TodoInsert/>
-            <TodoList/>
-        </TodoTemplate>)
-
-        ;
-}
+            <TodoInsert onInsert={onInsert}/>
+            <TodoList todos={todos} onRemove={onRemove} onToggle = {onToggle}/>
+        </TodoTemplate>
+    );
+};
 
 export default App;
