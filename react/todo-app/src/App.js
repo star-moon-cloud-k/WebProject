38cd1f60599ee771f9913c8e56cc385bd8d9cd5c
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-21 21:15:24 +0900

    Todo insert

--- react/todo-app/src/App.js
+++ react/todo-app/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoTemplate from "./components/TodoTemplate";
-const App = ()=> {
-  return <TodoTemplate>Todo App을 만들자</TodoTemplate>
+import TodoInsert from "./components/TodoInsert";
 
-  ;
+import {MdCached} from "react-icons/md";
+
+const App = () => {
+    return (
+        <TodoTemplate>
+            <TodoInsert/>
+        </TodoTemplate>)
+
+        ;
 }
 
 export default App;
