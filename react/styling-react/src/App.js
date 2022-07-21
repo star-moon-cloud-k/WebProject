af3af09cc6f5305ee852ceafaaa022c73857309b
star-moon-cloud-k <kspecial44@gmail.com>
2022-07-21 18:06:27 +0900

    Styled Component

--- react/styling-react/src/App.js
+++ react/styling-react/src/App.js
@@ -3,27 +3,14 @@ import './App.css';
 import {Component} from "react";
 import SassComponent from "./component/SassComponent";
 import CSSModule from "./component/CSSModule";
+import StyledComponent from "./component/StyledComponent";
 
 class App extends Component{
   render() {
     return (
         <div className="App">
-          <header>
-            <img src={logo} className="logo" alt="logo"/>
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-            <SassComponent/>
-          <CSSModule/>
+
+            <StyledComponent/>
         </div>
     );
   }
