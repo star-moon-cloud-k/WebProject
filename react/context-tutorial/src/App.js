a7fee5dd97be4e211efd7567f019a02a84b07ee9
star-moon-cloud-k <kspecial44@gmail.com>
2022-08-05 18:20:52 +0900

    selectColor;

--- react/context-tutorial/src/App.js
+++ react/context-tutorial/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ColorBox from './component/ColorBox';
 import ColorContext, {ColorProvider} from "./context/color";
+import SelectColors from "./component/SelectColors";
 
 const App = () =>{
   return (
       <ColorProvider>
           <div>
+              <SelectColors/>
               <ColorBox/>
           </div>
       </ColorProvider>
