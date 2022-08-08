6e24ac44180373820113c5074eb2742f28dc835a
star-moon-cloud-k <kspecial44@gmail.com>
2022-08-08 19:50:01 +0900

    loggerMiddleware

--- react/react-redux-middleware/src/index.js
+++ react/react-redux-middleware/src/index.js
@@ -5,9 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import rootReducer from "./modules";
 import {Provider} from "react-redux";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
+import loggerMiddleware from "./lib/loggerMiddleware";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
