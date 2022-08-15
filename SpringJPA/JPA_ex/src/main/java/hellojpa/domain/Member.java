efc2583ed3b7826e0802b2e38406d799f8b289e3
star-moon-cloud-k <kspecial44@gmail.com>
2022-08-15 18:58:08 +0900

    fetch type lazy

--- SpringJPA/JPA_ex/src/main/java/hellojpa/domain/Member.java
+++ SpringJPA/JPA_ex/src/main/java/hellojpa/domain/Member.java
@@ -22,7 +22,7 @@ public class Member extends BaseEntity{
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<Order>();
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
