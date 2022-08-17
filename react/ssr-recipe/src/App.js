5d7b50400df8862a6782215c7f6ae6587e707bcc
star-moon-cloud-k <kspecial44@gmail.com>
2022-08-17 21:08:15 +0900

    page init

--- react/ssr-recipe/src/App.js
+++ react/ssr-recipe/src/App.js
@@ -1,25 +1,20 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Menu from "./component/Menu";
+import RedPage from "./page/RedPage";
+import BluePage from "./page/BluePage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          fuck
-        </a>
-      </header>
+    <div>
+      <Menu />
+      <hr />
+      <Routes>
+        <Route path="/red" element={<RedPage />} />
+        <Route path="/blue" element={<BluePage />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
