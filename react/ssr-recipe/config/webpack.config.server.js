6a7cc8583a84b093daecdcd046fda7ad2421f6ae
star-moon-cloud-k <kspecial44@gmail.com>
2022-08-18 21:02:07 +0900

    server side rendering

--- react/ssr-recipe/config/webpack.config.server.js
+++ react/ssr-recipe/config/webpack.config.server.js
@@ -1,13 +1,129 @@
-const paths = require('./paths');
+const paths = require("./paths");
+const getCSSModuleLocalIndent = require("react-dev-utils/getCSSModuleLocalIdent");
+const nodeExternals = require("webpack-node-externals");
+const webpack = require("webpack");
+const getClientEnvironment = require("./env");
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
 
-module.export = {
-    mode: 'production',     //프로덕션 모드로 설정하여 최적화 옵션들을 활성화
-    entry: paths.ssrIndexJs,   //엔트리 경로
-    target: 'node', //node 환경에서 실행될 것이라는 점을 명시
-    output: {
-        path: paths.ssrBuild,   //빌드 경로
-        filename: 'server.js',  //파일 이름
-        chunkFilename: 'js/[name].chunk.js',    //청크 파일 이름
-        publicPath : paths.publicUrlOrPath, //정적 파일이 제공될 경로
-    }
-}
+const env = getClientEnvironment(paths.publicUrlOrPath.slice(0, -1));
+
+module.exports = {
+  mode: "production",
+  entry: paths.ssrIndexJs,
+  target: "node",
+  output: {
+    path: paths.ssrBuild,
+    filename: "server.js",
+    chunkFilename: "js/[name].chunk.js",
+    publicPath: paths.publicUrlOrPath,
+  },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.(js|mjs|jsx|ts|tsx)$/,
+            include: paths.appSrc,
+            loader: require.resolve("babel-loader"),
+            options: {
+              customize: require.resolve(
+                "babel-preset-react-app/webpack-overrides"
+              ),
+              presets: [
+                [
+                  require.resolve("babel-preset-react-app"),
+                  {
+                    runtime: "automatic",
+                  },
+                ],
+              ],
+              plugins: [
+                [
+                  require.resolve("babel-plugin-named-asset-import"),
+                  {
+                    loaderMap: {
+                      svg: {
+                        ReactComponent:
+                          "@svgr/webpack?-svgo,+titleProp,+ref![path]",
+                      },
+                    },
+                  },
+                ],
+              ],
+              cacheDirectory: true,
+              cacheCompression: false,
+              compact: false,
+            },
+          },
+          {
+            test: cssRegex,
+            exclude: cssModuleRegex,
+            loader: require.resolve("css-loader"),
+            options: {
+              importLoaders: 1,
+              modules: {
+                exportOnlyLocals: true,
+              },
+            },
+          },
+          {
+            test: cssModuleRegex,
+            loader: require.resolve("css-loader"),
+            options: {
+              importLoaders: 1,
+              modules: {
+                exportOnlyLocals: true,
+                getLocalIdent: getCSSModuleLocalIndent,
+              },
+            },
+          },
+          {
+            test: sassRegex,
+            exclude: sassModuleRegex,
+            use: [
+              {
+                loader: require.resolve("css-loader"),
+                options: {
+                  importLoaders: 3,
+                  modules: {
+                    exportOnlyLocals: true,
+                    getLocalIdent: getCSSModuleLocalIndent,
+                  },
+                },
+              },
+              require.resolve("sass-loader"),
+            ],
+          },
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: require.resolve("url-loader"),
+            options: {
+              emitFile: false,
+              limit: 10000,
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
+          {
+            loader: require.resolve("file-loader"),
+            exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+            options: {
+              emitFile: false,
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    modules: ["node_modules"],
+  },
+  externals: [
+    nodeExternals({
+      allowlist: [/@babel/],
+    }),
+  ],
+};
