7d932622eb5dfe729b1d3965a0d6c99f2737c032
star-moon-cloud-k <kspecial44@gmail.com>
2022-09-02 15:59:48 +0900

    jpql init

---
+++ SpringJPA/JPQL_ex/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,32 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        //일관적이 작업을 진행할 때 마다 EntityManager 를 만들어야함
+        EntityManager em = emf.createEntityManager();
+        //code 작성
+        //transaction 시작
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+
+            tx.commit();
+
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
