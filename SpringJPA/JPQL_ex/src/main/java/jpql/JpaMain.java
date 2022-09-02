a75e16d2d880f7d15ba288f61059fc8ba6e4ddaa
star-moon-cloud-k <kspecial44@gmail.com>
2022-09-02 17:06:19 +0900

    jpql test

--- SpringJPA/JPQL_ex/src/main/java/jpql/JpaMain.java
+++ SpringJPA/JPQL_ex/src/main/java/jpql/JpaMain.java
@@ -1,12 +1,9 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
+import jpql.domain.Member;
+
+import javax.persistence.*;
+
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -20,6 +17,16 @@ public class JpaMain {
         tx.begin();
         try {
 
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+//            TypedQuery<Member> query =
+             Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+            .setParameter("username", "member1")
+            .getSingleResult();
+            System.out.println("singleResult = " + result.getUsername());
             tx.commit();
 
         } catch (Exception e) {
