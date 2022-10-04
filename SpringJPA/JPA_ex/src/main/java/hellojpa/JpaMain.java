69bc4f430e320348488d886e080bac3dcafaafe9
star-moon-cloud-k <kspecial44@gmail.com>
2022-10-04 21:18:11 +0900

    jpa

--- SpringJPA/JPA_ex/src/main/java/hellojpa/JpaMain.java
+++ SpringJPA/JPA_ex/src/main/java/hellojpa/JpaMain.java
@@ -1,14 +1,14 @@
 package hellojpa;
 
 import hellojpa.domain.*;
-import org.hibernate.Hibernate;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
-import java.util.Set;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -21,43 +21,7 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = new Member();
-            member.setName("member1");
-            member.setHomeAddress(new Address("city", "street", "10000"));
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("===================================");
-            Member findMember = em.find(Member.class, member.getId());
-
-
-            System.out.println("===================================");
-//            List<Address> addressHistory = findMember.getAddressHistory();
-
-            //homeCity -> newCity
-            Address  a = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            //치킨 -> 한식
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-            System.out.println("================delete===================");
-            findMember.getAddressHistory().remove(new AddressEntity("old2", "street", "10000"));
-            System.out.println("===================================");
-            findMember.getAddressHistory().add(new AddressEntity("newCity11", "street", "10000"));
-
-            System.out.println("===================================");
             tx.commit();
 
         } catch (Exception e) {
